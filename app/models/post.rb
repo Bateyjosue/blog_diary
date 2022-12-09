a71493ffa8277bba3b617e6e46774e30d5bfaa2b
Bateyjosue <josuebatey19@gmail.com>
2022-12-09 10:53:39 +0200

    Update models to add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
