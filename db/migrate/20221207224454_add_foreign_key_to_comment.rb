45c6cbd91d16d4a902768847d437a7ee915dcefb
Bateyjosue <josuebatey19@gmail.com>
2022-12-09 10:55:24 +0200

    Add foreign Keys

--- db/migrate/20221207224454_add_foreign_key_to_comment.rb
+++ db/migrate/20221207224454_add_foreign_key_to_comment.rb
@@ -2,9 +2,12 @@ class AddForeignKeyToComment < ActiveRecord::Migration[7.0]
   def change
     remove_column :comments, :post_id, :integer
     remove_column :comments, :author_id, :integer
-    add_reference :comments, :post, null: false, foreign_key: true
+
+    add_reference :comments, :post, index: true
     add_reference :comments, :author, index: true
+
     add_foreign_key :comments, :users, column: :author_id
+    add_foreign_key :comments, :posts, column: :post_id
 
   end
 end
