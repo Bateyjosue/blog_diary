207e4153937c620bf27c22e505e7067ad90de4a9
Bateyjosue <josuebatey19@gmail.com>
2022-12-13 23:38:48 +0200

    Test users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before :each do
+      get "/users"
+    end
+
     it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
+    end
+
+    it 'should render index template' do 
+      expect(response).to render_template(:index)
+    end
+
+    it 'should return body of index template' do
+      expect(response.body).to include('<h1>')
     end
   end
 
   describe 'GET /show' do
+
+    user = User.create(name: 'josh', posts_counter: 0)
+    user.save
+
+    before :each do
+      get "/users/#{user.id}"
+    end
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render show template' do
+      expect(response).to render_template(:show)
+    end
+  
+    it 'should return the body of show template' do
+      expect(response.body).to include('Here is a specific user')
     end
   end
 end
