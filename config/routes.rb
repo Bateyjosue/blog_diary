9b97ca3933915b8a8eaa6317ec32504d528a49f6
Bateyjosue <josuebatey19@gmail.com>
2022-12-13 18:25:21 +0200

    Update routes to add users and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/', to: 'users#index'
+  get 'users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #create route for posts for  a users
+  get 'users/:id/posts', to: 'posts#index'
+  get 'users/:id/posts/:post_id', to: 'posts#show'
+
+  #create controller for posts for  a users
+  #rails g controller posts index show
+
+
+
 end
