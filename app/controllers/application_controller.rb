84a146901f7975b7d2315b1bc66413f60c1e843f
Bateyjosue <josuebatey19@gmail.com>
2022-12-17 19:00:28 +0200

    Add comment and likes controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Create a method called current_user in your ApplicationController to make current user data available to all controllers
+  def current_user
+    User.first
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
+    # @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post =  Post.new(params.require(:post).permit(:title, :text))
+    post.author = current_user
+
+    if post.save
+      redirect_to "/users/#{current_user.id}/posts/"
+    else
+      flash.now[:errors] = post.errors.full_messages
+      render :new
+    end
   end
 end
