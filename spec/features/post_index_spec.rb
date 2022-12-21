296ac10dc26486444a4889950c73a13c618bf848
Bateyjosue <josuebatey19@gmail.com>
2022-12-21 11:08:44 +0200

    Fix github linters

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 describe Post, type: :feature do
   before :each do
     @user1 = User.create(
@@ -94,3 +95,4 @@ describe Post, type: :feature do
     expect(current_path).to eq(user_post_path(@user1, @post1.id))
   end
 end
+# rubocop:enable Metrics/BlockLength
--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 describe Post, type: :feature do
   before :each do
     @user1 = User.create(
@@ -60,3 +61,4 @@ describe Post, type: :feature do
     expect(page).to have_content @comment1.text
   end
 end
+# rubocop:enable Metrics/BlockLength
