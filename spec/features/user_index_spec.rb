d6457f35af93a71b74bcb58db8a3e46303497ee1
shyusu4 <shyusupova4@gmail.com>
2023-01-03 12:45:54 +0500

    Add user tests

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+describe User, type: :feature do
+  before :all do
+    @user1 = User.create(name: 'Josh', photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                         bio: 'Here my story',
+                         posts_counter: 0)
+    @user2 = User.create(name: 'Meshu',
+                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                         bio: 'Here my story',
+                         posts_counter: 0)
+  end
+
+  it 'should display username' do
+    visit users_path(@user1)
+    expect(page).to have_content @user1.name
+  end
+
+  it 'should display users profile picture' do
+    visit users_path(@user1)
+    expect(page).to have_css("img[src*='#{@user1.photo}']")
+  end
+
+  it 'should display number of posts by given user' do
+    visit users_path(@user1)
+    expect(page).to have_content('0 Posts')
+    visit users_path(@user2)
+    expect(page).to have_content('0 Posts')
+  end
+
+  it 'should redirect to user show page' do
+    visit users_path(@user1)
+    click_link @user1.name.to_s
+    expect(current_path).to eq user_path(@user1)
+  end
+end
---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe User, type: :feature do
+  before :each do
+    @user1 = User.create(name: 'Ema',
+                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                         bio: 'Here my story',
+                         posts_counter: 3)
+  end
+
+  it 'should have username Ema' do
+    visit user_path(@user1)
+    expect(page).to have_content('Ema')
+  end
+
+  it 'should display number of posts by Ema' do
+    visit user_path(@user1)
+    Post.create(author: @user1, title: 'Introduction to rails', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    Post.create(author: @user1, title: 'Introduction to testing', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    Post.create(author: @user1, title: 'Introduction to Capybara', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    expect(page).to have_content('3 Posts')
+  end
+
+  it 'should display users profile picture' do
+    visit users_path(@user1)
+    expect(page).to have_css("img[src*='#{@user1.photo}']")
+  end
+
+  it 'should display user bio' do
+    visit user_path(@user1)
+    expect(page).to have_content(@user1.bio)
+  end
+
+  it 'should display first three posts' do
+    Post.create(author: @user1, title: 'Introduction to testing', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    Post.create(author: @user1, title: 'Introduction to Capybara', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    Post.create(author: @user1, title: 'Introduction to migration', text: 'Rails is a web', comments_counter: 0,
+                likes_counter: 0)
+    visit user_path(@user1)
+
+    expect(page).to have_content('Introduction to testing')
+    expect(page).to have_content('Introduction to testing')
+    expect(page).to have_content('Introduction to migration')
+  end
+
+  it 'should display button that allows to see all posts' do
+    visit user_path(@user1)
+
+    expect(page).to have_link('See all post')
+  end
+
+  it 'should redirect to user index page' do
+    visit user_path(@user1)
+    click_link('See all post')
+    expect(current_path).to eq(user_posts_path(@user1))
+  end
+
+  it 'should redirect to user show page' do
+    post = Post.create(author: @user1, title: 'Introduction to rails', text: 'Rails is a web', comments_counter: 0,
+                       likes_counter: 0)
+    visit user_posts_path(@user1)
+    click_link post.title.to_s
+    expect(current_path).to eq("/users/#{@user1.id}/posts/#{post.id}")
+  end
+end
