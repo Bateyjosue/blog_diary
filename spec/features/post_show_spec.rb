c52102789e21ea41d1e173b64af2ef4663b797da
Josh Batey <josuebatey19@gmail.com>
2023-01-03 17:22:12 +0200

    Fix lint

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+
+# rubocop:disable Metrics/BlockLength
 describe Post, type: :feature do
   before :each do
     @user1 = User.create(
@@ -52,3 +54,5 @@ describe Post, type: :feature do
     expect(page).to have_content @comment1.text
   end
 end
+
+# rubocop:enable Metrics/BlockLength
